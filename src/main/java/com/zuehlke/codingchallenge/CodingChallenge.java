2c6fe393aaddfe21da82adfdf5825f6f88047076
lonelyPorterCH <fabio.stuber@gmail.com>
2025-05-05 16:21:09 +0200

    implement possible solution

--- src/main/java/com/zuehlke/codingchallenge/CodingChallenge.java
+++ src/main/java/com/zuehlke/codingchallenge/CodingChallenge.java
@@ -1,6 +1,9 @@
 package com.zuehlke.codingchallenge;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CodingChallenge {
     private final SomeService someService = new SomeService();
@@ -10,6 +13,46 @@ public class CodingChallenge {
      * values comma separated.
      */
     public List<SomeObject> getMergedObjects() {
-        return someService.getData();
+        List<SomeObject> data = someService.getData();
+
+        //Grouping SomeObject by Identifier, each List entry is another list of entries with the same
+        // identifier (but different values)
+        Collection<List<SomeObject>> groupedObjects = data
+            .stream()
+            .collect(Collectors.groupingBy(SomeObject::identifier, Collectors.toList()))
+            .values();
+        return mapGroupedObjects(groupedObjects);
+    }
+
+    /**
+     * maps the List of grouped Objects into a single List of SomeObject with joined values.
+     * Sports the SomeObject by identifier (descending)
+     */
+    private List<SomeObject> mapGroupedObjects(Collection<List<SomeObject>> groupedObjects) {
+        return groupedObjects.stream().map(objectList -> {
+            //Index is not important since all of them have the same values (except the one we want to merge)
+            //and the first entry should always be present
+            SomeObject someObject = objectList.get(0);
+
+            String mergedValues = objectList.stream()
+                .map(SomeObject::value)
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+            return copyWithValue(someObject, mergedValues);
+        })
+        .sorted(Comparator.comparing(SomeObject::identifier))
+        .toList();
+    }
+
+    private SomeObject copyWithValue(SomeObject someObject, String mergedValues) {
+        return new SomeObject(
+            someObject.identifier(),
+            mergedValues,
+            someObject.valueA(),
+            someObject.valueB(),
+            someObject.valueC()
+        );
     }
 }
