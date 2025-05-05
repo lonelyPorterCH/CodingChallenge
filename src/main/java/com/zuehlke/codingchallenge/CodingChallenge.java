0b5f1f4e96ef8f1535bfd17b99225d8bc84fb094
lonelyPorterCH <fabio.stuber@gmail.com>
2025-05-05 16:01:14 +0200

    Javadoc

--- src/main/java/com/zuehlke/codingchallenge/CodingChallenge.java
+++ src/main/java/com/zuehlke/codingchallenge/CodingChallenge.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class CodingChallenge {
     private final SomeService someService = new SomeService();
 
+    /**
+     * Returns all SomeObject, SomeObject with the same Identifier are merged with value containing all
+     * values comma separated.
+     */
     public List<SomeObject> getMergedObjects() {
         return someService.getData();
     }
